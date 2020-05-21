af392b88dbb4a0ab34b68b41fbe319552195bc7c
Andre Dippenaar <andre.pierre.dippenaar@gmail.com>
2020-05-21 13:30:08 +0200

    TextFields TextEditing Contollers added

--- lib/main.dart
+++ lib/main.dart
@@ -37,26 +37,80 @@ class MyHomePage extends StatelessWidget {
     ),
   ];
 
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xff121212),
       appBar: AppBar(
         title: Text("Expense Tracker"),
+        backgroundColor: Color(0xff121212),
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: <Widget>[
           Card(
             child: Container(
               width: double.infinity,
+              color: Color(0xff03dac6),
               height: 200,
               child: Center(child: Text("Chart")),
             ),
             elevation: 5,
           ),
+          Card(
+            color: Color(0xff1f1b24),
+            elevation: 5,
+            child: Container(
+              padding: EdgeInsets.all(12),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: <Widget>[
+                  TextField(
+                    controller: titleController,
+                    decoration: InputDecoration(
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Color(0xffbb86fc)),
+                          //  when the TextFormField in focused
+                        ),
+                        labelText: 'Title',
+                        labelStyle: TextStyle(color: Color(0xffbb86fc)),
+                        filled: true,
+                        fillColor: Color(0xff1f1b24)),
+                    style: TextStyle(color: Color(0xff03dac6)),
+                  ),
+                  TextField(
+                    controller: amountController,
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Color(0xffbb86fc)),
+                          //  when the TextFormField in focused
+                        ),
+                        labelText: 'Amount',
+                        labelStyle: TextStyle(color: Color(0xffbb86fc)),
+                        filled: true,
+                        fillColor: Color(0xff1f1b24)),
+                    style: TextStyle(color: Color(0xff03dac6)),
+                  ),
+                  FlatButton(
+                      onPressed: () {
+                        print(titleController.text);
+                        print(amountController.text);
+                      },
+                      child: Text(
+                        "Add Transaction",
+                        style: TextStyle(color: Color(0xffbb86fc)),
+                      ))
+                ],
+              ),
+            ),
+          ),
           Column(
             children: transactions.map((tx) {
               return Card(
+                color: Color(0xff1f1b24),
                 child: Row(
                   children: <Widget>[
                     Container(
@@ -66,13 +120,14 @@ class MyHomePage extends StatelessWidget {
                       alignment: Alignment.center,
                       width: 140,
                       decoration: BoxDecoration(
-                          border: Border.all(color: Colors.blue, width: 2)),
+                          border:
+                              Border.all(color: Color(0xffbb86fc), width: 2)),
                       child: Text(
                         "R${tx.amount}",
                         style: TextStyle(
                             fontWeight: FontWeight.bold,
                             fontSize: 20,
-                            color: Colors.blue),
+                            color: Color(0xffbb86fc)),
                       ),
                     ),
                     Column(
@@ -81,7 +136,9 @@ class MyHomePage extends StatelessWidget {
                         Text(
                           tx.title,
                           style: TextStyle(
-                              fontWeight: FontWeight.bold, fontSize: 18),
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                              color: Colors.white),
                         ),
                         Text(
                           DateFormat.yMMMd().format(tx.date),
